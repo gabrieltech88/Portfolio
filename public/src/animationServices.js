3e8b8eaeaddc0b4d764c140ddaeca7b7445c1697
gabrieltech88 <gabrieltech209@gmail.com>
2025-03-20 13:38:10 -0300

    toogle e animações adicionadas

---
+++ public/src/animationServices.js
@@ -0,0 +1,18 @@
+function checkServicesVisibility() {
+    const servicesSection = document.getElementById("services");
+    const servicesCards = document.querySelectorAll('.items-list-services');
+    const servicesPosition = servicesSection.getBoundingClientRect();
+  
+    if (servicesPosition.top <= window.innerHeight && servicesPosition.bottom >= 0) {
+      servicesCards.forEach((card, index) => {
+        // Aplica a classe 'show' com um delay para cada card
+        setTimeout(() => {
+          card.classList.add('show');
+        }, index *10); // 200ms entre os cards
+      });
+    }
+  }
+  
+  // Detecta o evento de rolagem e verifica se a seção de serviços está visível
+  window.addEventListener("scroll", checkServicesVisibility);
+  checkServicesVisibility();  // Verifica na primeira carga
